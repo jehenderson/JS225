40883375613632ca5a203f1a46ac4a9d4abb60dd
James Henderson <james.e.henderson269@gmail.com>
2020-06-23 19:25:44 -0700

    practice

--- Constructors/defineProperties.js
+++ Constructors/defineProperties.js
@@ -1,28 +1,13 @@
-var obj = {
-  name: 'Obj',
-};
-
-Object.defineProperties(obj, {
-  age: {
-    value: 30,
-    writable: false,
-  },
-});
-
-console.log(obj.age); // => 30
-obj.age = 32;
-console.log(obj.age); // => 30
-
-// Using this method, create a function that constructs a new object with a
-// log method that is read-only. The log method will use console.log to output
-// the name property on itself.
+// Using this method, create a function that constructs a new object with a log
+// method that is read-only. The log method will use console.log to output the
+// name property on itself.
 
 function newPerson(name) {
-  var obj = {name: name};
+  let obj = {}
   Object.defineProperties(obj, {
     log: {
       value: function() {
-        console.log(this.name);
+        console.log(name);
       },
       writable: false,
     },
@@ -30,7 +15,7 @@ function newPerson(name) {
   return obj;
 }
 
-var me = newPerson('Shane Riley');
+let me = newPerson('Shane Riley');
 me.log();     // => Shane Riley
 me.log = function() { console.log('Amanda Rose'); };
 me.log();     // => Shane Riley
